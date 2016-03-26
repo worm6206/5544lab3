f92f1b35a2c342648b225ff10e9015e1a81dc8b5
Kevin Yen <worm6206@gmail.com>
2016-03-26 15:58:16 -0400

    auto update script

---
+++ git.sh
@@ -0,0 +1,9 @@
+#update script
+git add .
+git status # to see what changes are going to be commited
+git commit -m "auto update script"
+git push origin master
+git checkout gh-pages # go to the gh-pages branch
+git rebase master # bring gh-pages up to date with master
+git push origin gh-pages # commit the changes
+git checkout master # return to the master branch
